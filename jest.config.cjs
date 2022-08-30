510104ebac4a84b3183a09e1603a9e9669c3417d
MitsukiIwamura <boc.fsfb@gmail.com>
2022-08-31 03:09:28 +0900

    jest導入

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
